6d72faef162c76655e87cc80139949fc8dd5d026
meharoof <meharoof@qburst.com>
2016-05-26 17:22:15 +0530

    Initial setup.

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) { //eslint-disable-line no-unused-vars
+  res.render('index', {
+    title: 'Express'
+  });
+});
+
+module.exports = router;
