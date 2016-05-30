a601ccfd89c912719ff012509998b26bb77b7e7a
Nithin.P <nithinp@qburst.com>
2016-05-30 21:00:58 +0530

    aws file upload

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,49 @@
 
 var express = require('express');
 var router = express.Router();
+var multer = require('multer');
+var path = require('path');
+var fs = require('fs');
+var bluebird = require('bluebird');
+var xlsx = require('xlsx');
+
+var awsUpload = require('../utilities/aws-upload.js');
+
+var upload = multer({
+  dest: 'uploads/'
+});
+
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {
-    title: 'Express'
+  var filePath = path.resolve(__dirname + '/../public/home.html');
+  fs.createReadStream(filePath).pipe(res);
+});
+
+router.post('/', upload.single('file'), function (req, res, next) {
+  //console.log(req.file);
+  var categoryId = 'Category ID ';
+  var category = 'Category';
+  var workbook = xlsx.readFile(req.file.path);
+  //var count =0;
+  bluebird.map(workbook.SheetNames, function (item) {
+    var jsonArray = xlsx.utils.sheet_to_json(workbook.Sheets[item]);
+    return bluebird.map(jsonArray, function (obj) {
+
+      if (obj !== undefined && obj[categoryId] !== undefined && obj[category] !== undefined) {
+        var filePath = 'JSON_Files/' + obj[category].trim().replace(/\s>\s/g, '/');
+        var jsonFilePath = filePath + '/' + obj[categoryId] + '.json';
+        delete obj[categoryId];
+        delete obj[category];
+        awsUpload(obj, jsonFilePath);
+      }
+
+    });
   });
+  //console.log(count);
+  var filePath = path.resolve(__dirname + '/../public/home.html');
+  fs.createReadStream(filePath).pipe(res);
+
 });
 
 module.exports = router;
---
+++ utilities/aws-upload.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var k = require('../config/constants.js');
+var AWS = require('aws-sdk');
+
+// var bucketName = 'my_bucket';
+var config = {
+  s3ForcePathStyle: true,
+  accessKeyId: k.AWS.ACCESS_KEY_ID,
+  secretAccessKey: k.AWS.SECRET_ACCESS_KEY,
+  endpoint: new AWS.Endpoint(k.AWS.ENDPOINT)
+};
+var client = new AWS.S3(config);
+
+module.exports = function (jsonObj, jsonFilePath) {
+  var params = {
+    Bucket: k.AWS.BUCKET_NAME
+  };
+  params.Key = jsonFilePath;
+  params.Body = JSON.stringify(jsonObj);
+  return client.putObject(params).promise().then(function (data) {
+    // Uploading done.
+  });
+};
